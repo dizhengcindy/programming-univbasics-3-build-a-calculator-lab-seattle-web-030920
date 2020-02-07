0113b8a45baf1d069dc49b923a4ded6a2cbf6d1a
Di Zheng <dizhengcindy@gmail.com>
2020-02-07 06:18:37 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,35 @@
 def addition(num1, num2)
-
+sum = num1+num2
+puts"#{sum}"
+sum
 end
 
 def subtraction(num1, num2)
-
+df = num1-num2
+puts"#{df}"
+df
 end
 
 def division(num1, num2)
-
+div = num1/num2
+puts "#{div}"
+div
 end
 
 def multiplication(num1, num2)
-
+mul = num1*num2
+puts "#{mul}"
+mul
 end
 
 def modulo(num1, num2)
-
+remaider = num1%num2
+puts"#{remaider}"
+remaider
 end
 
 def square_root(num)
-
+sr = Math.sqrt(num)
+puts "#{sr}"
+sr
 end
